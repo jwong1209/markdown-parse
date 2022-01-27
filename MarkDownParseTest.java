bc234ba3646845584915636308ed8d7c6806c1d6
jwong1209 <97646123+jwong1209@users.noreply.github.com>
2022-01-26 19:01:03 -0800

    endOfLab

--- MarkDownParseTest.java
+++ MarkDownParseTest.java
@@ -1,9 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.List;
+import java.io.IOException;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFileOne() throws IOException{
+
+        String contentsTest = Files.readString(Path.of("test-file.md"));
+        assertEquals(List.of("https://something.com", "some-page.html"),
+                MarkdownParse.getLinks(contentsTest));
     }
 }
