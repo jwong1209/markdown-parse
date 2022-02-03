881603d581828578955ffe7248579a8166743e70
jwong1209 <97646123+jwong1209@users.noreply.github.com>
2022-02-02 19:46:31 -0800

    fixNames

--- MarkDownParseTest.java
+++ MarkDownParseTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkDownParseTest.java
 //java -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore  MarkDownParseTest
-public class MarkDownParseTest {
+public class MarkdownParseTest {
     @Test
     public void addition() throws IOException {
         // assertEquals(2, 1 + 1);
@@ -20,13 +20,13 @@ public class MarkDownParseTest {
         String contentsTest3 = Files.readString(Path.of("./test-file4.md"));
 
         assertEquals(List.of("https://something.com", "some-page.html"), 
-            MarkDownParse.getLinks(contentsTest));
+            MarkdownParse.getLinks(contentsTest));
         assertEquals(List.of("https://try.com", "working.html"), 
-            MarkDownParse.getLinks(contentsTest1));
+            MarkdownParse.getLinks(contentsTest1));
         assertEquals(List.of("https://anything.com", "another-page.html", "another-page.html",
          "a-page.html"), 
-            MarkDownParse.getLinks(contentsTest2));
+            MarkdownParse.getLinks(contentsTest2));
         assertEquals(List.of("link.com"), 
-            MarkDownParse.getLinks(contentsTest3));
+            MarkdownParse.getLinks(contentsTest3));
     }
 }
