764823036cfdc2bb18a6fc5949a08e40aef90d7b
jwong1209 <97646123+jwong1209@users.noreply.github.com>
2022-02-09 17:48:34 -0800

    Makefile

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-
+//ssh cs15lwi22zz@ieng6.ucsd.edu
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 //java -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
@@ -14,10 +14,10 @@ public class MarkdownParseTest {
     public void addition() throws IOException {
         // assertEquals(2, 1 + 1);
 
-        String contentsTest = Files.readString(Path.of("test-file.md"));
-        String contentsTest1 = Files.readString(Path.of("test-file2.md"));
-        String contentsTest2 = Files.readString(Path.of("test-file3.md"));
-        String contentsTest3 = Files.readString(Path.of("test-file4.md"));
+        String contentsTest = Files.readString(Path.of("C:/Users/jwong/OneDrive/Documents/GitHub/markdown-parse/test-file.md"));
+        String contentsTest1 = Files.readString(Path.of("C:/Users/jwong/OneDrive/Documents/GitHub/markdown-parse/test-file2.md"));
+        String contentsTest2 = Files.readString(Path.of("C:/Users/jwong/OneDrive/Documents/GitHub/markdown-parse/test-file3.md"));
+        String contentsTest3 = Files.readString(Path.of("C:/Users/jwong/OneDrive/Documents/GitHub/markdown-parse/test-file4.md"));
 
         assertEquals(List.of("https://something.com", "some-page.html"), 
             MarkdownParse.getLinks(contentsTest));
